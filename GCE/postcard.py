ded31740205c966fc4c12fdf7fb8620fadece1cd
David Carr <dcarr@scienceprousa.com>
2013-09-07 19:40:30 +0000

    working on post

--- GCE/postcard.py
+++ GCE/postcard.py
@@ -1,27 +1,34 @@
-from flask import  Flask
-import vidmaker
+from flask import  Flask, request
+#import vidmaker
 import datetime
+import base64
 app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def root():
 	return app.send_static_file('index.html')
 
-@app.route('/create', methods=['POST'])
+@app.route('/create', methods=['POST','GET'])
 def create():
 	if request.method == 'POST':
-		token = request.form['token']
-		userid = request.form['userid']
-		images = request.form['image_array']
-		for image in images:
-			
+		data = request.data
+		#token = request.form['accessToken']
+		#eventid = request.form['eventId']
+		#userid = request.form['gPlusId']
+		#image_array = request.form['images']
+		#imgindex=0
+		#for image in image_array:
+	#		f = open('glass'+imgindex+'.jpg', 'w')
+#			f.write(base64.decodestring(image))
+	#		f.close()		
 		#vid_dir = '/static/'+userid+'/'+datetime.datetime.now()
 		#os.mkdir(vid_dir)
 		#upack files from post request
 		#vidmaker.process(vid_dir, token, userid)
-		return 'Success'
+	#	return 'Success'+eventid
+		return data
 	else:
 		return 'What did the fox say?'
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=80)
+	app.run(host='0.0.0.0', port=80, debug=True)
